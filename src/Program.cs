471207882f0567764499eeb421191f726f8dc2db
Hallvard Vasstveit <hallvard@halv.io>
2018-10-09 18:38:34 +0200

    Rewrite because of changes in GnomeOnlineAccounts

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,8 @@ using Tmds.DBus;
 using OnlineAccounts.DBus;
 using MailKit.Net.Imap;
 using MailNotifier.AccountRepositories;
+using MailKit;
+using MailKit.Security;
 
 namespace MailNotifier
 {
@@ -13,30 +15,20 @@ namespace MailNotifier
         {
             Console.WriteLine("Getting gnome accounts...");
             var gnomeRepo = new GnomeOnlineAccounts();
-            var accounts = await gnomeRepo.GetImapAccounts();
+            var accounts = await gnomeRepo.GetAllMailAddresses();
 
             foreach (var account in accounts)
             {
-                Console.WriteLine($"{account.Host}");
-                Console.WriteLine($" - Username: {account.Username}");
-                Console.WriteLine($" - Port: {account.Port} ");
-                Console.WriteLine($" - UseSSL: {account.UseSsl} ");
-                Console.WriteLine(" ");
+                Console.WriteLine($"Connecting to: {account}");
+                using (var client = await gnomeRepo.GetImapClient(account))
+                {
+                    var inbox = client.Inbox;
+                    await inbox.OpenAsync(FolderAccess.ReadOnly);
+                    Console.WriteLine($"Total messages in inbox: {inbox.Count}\n");
+
+                    await client.DisconnectAsync(true);
+                }
             }
-
-            Console.WriteLine("\n --- \nPasswords:");
-            var sessionConnection = Connection.Session;
-
-            // Works
-            var passwordManager = sessionConnection.CreateProxy<IPasswordBased>("org.gnome.OnlineAccounts", "/org/gnome/OnlineAccounts/Accounts/account_1537887570_3");
-
-            Console.WriteLine(await passwordManager.GetPasswordAsync("imap-password"));
-
-            using (var client = new ImapClient())
-            {
-                //client.
-            }
-
         }
     }
 }
